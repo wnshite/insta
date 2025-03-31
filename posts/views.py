381f68aa7c5e56a19a1194bf689e752997d3d889
seunghyun <tmdgus_skfk@naver.com>
2025-03-31 11:54:33 +0900

    post create 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
@@ -9,3 +11,16 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'create.html', context)
