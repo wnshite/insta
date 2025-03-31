5715b076cbb0ccbc3df32f8ef4159441662979e8
seunghyun <tmdgus_skfk@naver.com>
2025-03-31 12:11:14 +0900

    image resize 기능 추가

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 # Create your models here.
 class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='image')
+    # image = models.ImageField(upload_to='image')
+    image = ResizedImageField(
+        size=[500, 500],
+        crop=['middle', 'center'],
+        upload_to='image/%Y/%m'
+    )
