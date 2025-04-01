a1c4ae2bcf62bbdc9c5449684f96070fe087f380
seunghyun <tmdgus_skfk@naver.com>
2025-04-01 14:08:06 +0900

    follow 기능을 위한 모델링

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,7 @@ class User(AbstractUser):
         crop=['middle', 'center'],
         upload_to='profile',
     )
+
+    followings = models.ManyToManyField('self', related_name='followers', symmetrical=False)  # 내가 팔로우를 한 사람들.
+    # followers = 
+
